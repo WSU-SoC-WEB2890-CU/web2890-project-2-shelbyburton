e374e75863c8d8c7e32ff6600c02edd0ed97bb28
shelbyburton <89862418+shelbyburton@users.noreply.github.com>
2023-03-24 19:01:20 -0600

    First commit

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,17 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+//Search Engine
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=site%3A+';
+const site = 'https://endearing-lily-476e26.netlify.app/';
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + site + '+' + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+
+f.addEventListener('submit', submitted);
